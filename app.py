a330f87fd9b16dea3f8e0b7c1cc8bde937c842da
yaseenabdulghany <yaseen.abdulghany@gmail.com>
2025-08-27 04:03:04 +0300

    First commit

---
+++ app.py
@@ -0,0 +1,12 @@
+def main():
+    try:
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+
+        print("The maximum number is:", max(num1, num2))
+    except ValueError:
+        print("Please enter valid numbers.")
+
+if __name__ == "__main__":
+    main()
+
